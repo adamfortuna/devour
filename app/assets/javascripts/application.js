3e7fd7a2d0112f035720b63b5d7aab7371b023fe
Adam Fortuna <adam@fortuna.name>
2016-08-31 00:26:13 -0400

    Synchronized map and listing with infowindows

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,14 @@
+//= require jquery
 //= require lodash/dist/lodash.core
+//= require jquery.scrollTo/jquery.scrollTo
+
+function scrollToLocation(slug) {
+  var id = 'location-map-' + slug,
+      el = $('[data-id='+id+']')
+  $('.cards').scrollTo(el, 800, {
+    over: { top: -0.01}
+  });
+}
 
 window.initMap = function initMap() {
   var bounds = new google.maps.LatLngBounds();
@@ -6,19 +16,34 @@ window.initMap = function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'));
 
-  var lngs = _.map(maps.locations, function(location) {
-    var myLatLng = {
+  var info;
+
+  var lngs = _.map(maps.locations, function(location, index) {
+    var point = new google.maps.LatLng({
       lat: location.address.lat,
       lng: location.address.lng
-    };
-
-    var point = new google.maps.LatLng({lat: location.address.lat, lng: location.address.lng});
+    });
     bounds.extend(point);
-
     var marker = new google.maps.Marker({
       map: map,
-      position: myLatLng,
-      title: location.name
+      position: point,
+      label: (index+1).toString()
+    });
+
+    marker.addListener('click', function() {
+      scrollToLocation(location.slug);
+
+      if (info) {
+        info.close();
+      }
+
+      info = new google.maps.InfoWindow({
+        position: point,
+        maxWidth: 200,
+        content: "<h3>"+location.name+"</h3>"
+      });
+
+      info.open(map, marker);
     });
 
     return location.address.lng;
