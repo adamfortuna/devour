c0361dcbf55ecb4f262fa6dbe543085f4e7e7112
Adam Fortuna <adam@fortuna.name>
2016-08-31 22:44:55 -0400

    Backup system for finding address

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,23 @@ class HomeController < ApplicationController
   def contact; end
 
   def locate
-    if session[:geo_location]['lat'] && session[:geo_location]['lng']
+    redirect_to map_path(closest_map)
+  end
+
+  private
+
+  def closest_map
+    if session[:geo_location].is_a? Hash
       lat_Lng = Geokit::LatLng.new(session[:geo_location]['lat'], session[:geo_location]['lng'])
       if address = Address.within(250, origin: lat_Lng).first
-        redirect_to map_path(address.location.map)
-      else
-        redirect_to map_path(Map.first)
+        map = address.location.maps.first
       end
     else
-      redirect_to map_path(Map.first)
+      if address = Address.within(250, origin: session[:geo_location]).first
+        map = address.location.maps.first
+      end
     end
+
+    map || Map.first
   end
 end
